dd005a8240cd232bd8db76be76ab98ab3ad6d703
amansahota-enuke <aman.sahota@enukesoftware.com>
2021-09-03 10:42:07 +0530

    Navbar

--- src/Components/Common/Dropdown.js
+++ src/Components/Common/Dropdown.js
@@ -7,20 +7,50 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function Dropdown({ name }) {
+    const menuItems = [
+        { for: "Info", name: "Resources", type: "link", path: "/home" },
+        { for: "Info", name: "Blog", type: "link", path: "/home" },
+        { for: "Info", name: "FAQ", type: "link", path: "/home" },
+        {
+            for: "For Patients",
+            name: "login",
+            type: "url",
+            path: process.env.REACT_APP_VERBENA_PATIENT,
+        },
+        {
+            for: "For Patients",
+            name: "register",
+            type: "url",
+            path: `${process.env.REACT_APP_VERBENA_PATIENT}/signup`,
+        },
+        {
+            for: "For Providers",
+            name: "login",
+            type: "url",
+            path: process.env.REACT_APP_VERBENA_PATIENT,
+        },
+        {
+            for: "For Providers",
+            name: "Why Verbena",
+            type: "link",
+            path: "/home",
+        },
+        {
+            for: "For Providers",
+            name: "register",
+            type: "url",
+            path: `${process.env.REACT_APP_VERBENA_PATIENT}/signup`,
+        },
+    ];
+
     return (
         <Menu as="div" className="relative">
             {({ open }) => (
                 <>
                     <div>
                         <Menu.Button className="inline-flex justify-center items-center">
-                            <img
-                                src="/images/user.png"
-                                className="mr-2 w-10"
-                                alt=""
-                                title=""
-                            />
-                            Test
+                            {name}
                         </Menu.Button>
                     </div>
 
@@ -38,23 +68,43 @@ export default function Example() {
                             static
                             className="origin-top-right absolute right-0 w-24 rounded-md shadow-lg bg-white"
                         >
-                            <div className="">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <Link
-                                            to="/home/profile"
-                                            className={classNames(
-                                                active
-                                                    ? "bg-gray-100 text-gray-900"
-                                                    : "text-gray-700",
-                                                "block px-4 py-2 text-sm"
-                                            )}
-                                        >
-                                            Profile
-                                        </Link>
-                                    )}
-                                </Menu.Item>
-                            </div>
+                            {menuItems.map((item,index) => {
+                                if (item.for === name) {
+                                    return (
+                                        <Menu.Item key={index}>
+                                            {({ active }) =>
+                                                item.type === "link" ? (
+                                                    <Link
+                                                        to={item.path}
+                                                        className={classNames(
+                                                            active
+                                                                ? "bg-gray-100 text-gray-900"
+                                                                : "text-gray-700",
+                                                            "block px-4 py-2 text-sm"
+                                                        )}
+                                                    >
+                                                        {item.name}
+                                                    </Link>
+                                                ) : (
+                                                    <a
+                                                        href={item.path}
+                                                        className={classNames(
+                                                            active
+                                                                ? "bg-gray-100 text-gray-900"
+                                                                : "text-gray-700",
+                                                            "block px-4 py-2 text-sm"
+                                                        )}
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                )
+                                            }
+                                        </Menu.Item>
+                                    );
+                                }
+
+                                return null;
+                            })}
                         </Menu.Items>
                     </Transition>
                 </>
--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import React, { Fragment, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import $ from "jquery";
+import Dropdown from "./Dropdown";
 
-const Header = ({ handleToggle }) => {
+const Header = () => {
     useEffect(() => {
         $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
@@ -12,13 +13,37 @@ const Header = ({ handleToggle }) => {
             }
         });
     });
-    
+
+    const classNames = (...classes) => {
+        return classes.filter(Boolean).join(" ");
+    };
+
+    const [navigation, setNavigation] = useState([
+        { name: "Home", type: "link", path: "/home", current: true },
+        { name: "About", type: "link", path: "/about", current: false },
+        { name: "Services", type: "link", path: "/services", current: false },
+        { name: "Info", type: "drop", path: "#", current: false },
+        { name: "Contact", type: "link", path: "/home", current: false },
+        { name: "For Patients", type: "drop", path: "#", current: false },
+        { name: "For Providers", type: "drop", path: "#", current: false },
+    ]);
+
+    const handleNavigation = (selected) => {
+        const updatedNavigation = [];
+        navigation.forEach((item) => {
+            if (item.name === selected.name) {
+                updatedNavigation.push({ ...item, current: true });
+            } else {
+                updatedNavigation.push({ ...item, current: false });
+            }
+        });
+
+        setNavigation(updatedNavigation);
+    };
+
     return (
         <>
-            <div
-                id="header"
-                className="fixed top-0 w-full z-50"
-            >
+            <div id="header" className="fixed top-0 w-full z-50">
                 <div className="container mx-auto px-4 sm:px-6">
                     <div className="flex justify-between items-center py-4 md:space-x-10">
                         <div className="flex justify-start items-center">
@@ -30,19 +55,54 @@ const Header = ({ handleToggle }) => {
                                     alt=""
                                 />
                             </NavLink>
-                            <span
-                                className="toggle-menu ml-8 cursor-pointer"
-                                onClick={(e) => {
-                                    handleToggle();
-                                }}
-                            >
-                                <img
-                                    className="w-8"
-                                    src="/images/bars.png"
-                                    alt=""
-                                    title=""
-                                />
-                            </span>
+                        </div>
+                        <div className="hidden sm:block sm:ml-6">
+                            <div className="flex space-x-4">
+                                {navigation.map((item) => (
+                                    <Fragment key={item.name}>
+                                        {item.type === "link" ? (
+                                            <Link
+                                                to={item.path}
+                                                className={classNames(
+                                                    item.current
+                                                        ? "bg-gray-900 text-white"
+                                                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                    "px-3 py-2 rounded-md text-sm font-medium"
+                                                )}
+                                                aria-current={
+                                                    item.current
+                                                        ? "page"
+                                                        : undefined
+                                                }
+                                                onClick={() =>
+                                                    handleNavigation(item)
+                                                }
+                                            >
+                                                {item.name}
+                                            </Link>
+                                        ) : (
+                                            <span
+                                                className={classNames(
+                                                    item.current
+                                                        ? "bg-gray-900 text-white"
+                                                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                    "px-3 py-2 rounded-md text-sm font-medium"
+                                                )}
+                                                aria-current={
+                                                    item.current
+                                                        ? "page"
+                                                        : undefined
+                                                }
+                                                onClick={() =>
+                                                    handleNavigation(item)
+                                                }
+                                            >
+                                                <Dropdown name={item.name} />
+                                            </span>
+                                        )}
+                                    </Fragment>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
