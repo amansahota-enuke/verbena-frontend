7401c2a56606df30a528e1fc3eaabaa24d174218
aman sahota <aman.sahota@enukesoftware.com>
2021-10-21 17:59:58 +0530

    fix

--- src/Components/Home/Intro.js
+++ src/Components/Home/Intro.js
@@ -6,9 +6,9 @@ function Intro() {
             <span  data-aos-duration="1000"data-aos="slide-left" className="xl:absolute lg:absolute md:relative sm:relative relative right-0 top-0"><img src="/images/explore-bg.png" alt="" title=""/></span>
             <div className="flex justify-start">
                 <div data-aos-duration="1000" data-aos="slide-right" className="health-explore-content">
-                    <h3 className="font-72 hepta-bold question-title-color leading-none">Your Doctor.</h3>
-                    <h3 className="font-72 hepta-bold question-title-color leading-none">Your Care.</h3>
-                    <h3 className="font-72 hepta-bold question-title-color leading-none mb-10">Your Time.</h3>
+                    <h3 className="font-72 hepta-semibold question-title-color leading-none">Your Doctor.</h3>
+                    <h3 className="font-72 hepta-semibold question-title-color leading-none">Your Care.</h3>
+                    <h3 className="font-72 hepta-semibold question-title-color leading-none mb-10">Your Time.</h3>
                     <p className="mont-font mont-regular font-20 mb-10">
                         We are a comprehensive women’s health platform offering
                         digital care and telemedicine services to patients,
