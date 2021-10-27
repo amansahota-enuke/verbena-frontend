961d71f1b8a7a6f170181d2eff4aee771f431e3b
aman sahota <aman.sahota@enukesoftware.com>
2021-10-27 17:09:15 +0530

    fix

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -49,11 +49,11 @@ export default function Header() {
         if (location.pathname === "/Why-Verbena") {
             return handleNavigation(navigation[6]);
         }
-        if (location.pathname) {
-            return handleNavigation(
-                navigation.find((ele) => ele.path === location.pathname)
-            );
-        }
+        // if (location.pathname) {
+        //     return handleNavigation(
+        //         navigation.find((ele) => ele.path === location.pathname)
+        //     );
+        // }
     }, [location]);
 
     return (
