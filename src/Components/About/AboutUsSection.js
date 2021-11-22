4b0c4732ee5f0a92119929ae03162330a959b6b1
Gaurav Thakur <gauravthakur262@gmail.com>
2021-11-22 12:39:10 +0530

    Design changes

--- src/Components/About/AboutUsSection.js
+++ src/Components/About/AboutUsSection.js
@@ -5,10 +5,10 @@ function AboutUsSection() {
     return (
         <div className="about-us-wrapper">
             <div className=" grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 gap-6 items-center">
-                <div className="about-us-img">
+                <div data-aos-duration="1000"data-aos="slide-right" className="about-us-img">
                     <img src="/images/about-us-img.png" alt="" title="" />
                 </div>
-                <div className="about-us-content">
+                <div data-aos-duration="1000" data-aos="slide-left" className="about-us-content">
                     <h2 className="font-72 question-title-color mb-10 hepta-semibold leading-none">
                         About Us.
                     </h2>
